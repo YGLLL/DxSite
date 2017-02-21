f117874f85fb2ebd1ed336abffa15bca69476b46
DXkite <DXkite@163.com>
2017-02-21 16:31:33 +0800

    发送，阅读，删除

--- app/modules/default/src/cn/atd3/Api.php
+++ app/modules/default/src/cn/atd3/Api.php
@@ -1,20 +1,7 @@
 <?php
 namespace cn\atd3;
 
-class ApiException extends \Exception implements \JsonSerializable
-{
-    protected $name,$msg;
-    public function __construct(string $name, string $message)
-    {
-        $this->name=$name;
-        $this->msg=$message;
-        parent::__construct($name.':'.$message);
-    }
-    public function jsonSerialize()
-    {
-        return ['error'=>$this->name, 'message'=>$this->msg];
-    }
-}
+
 
 class Api
 {
--- app/modules/default/src/cn/atd3/response/api/Msg.php
+++ app/modules/default/src/cn/atd3/response/api/Msg.php
@@ -12,9 +12,11 @@ use suda\core\Cookie;
 use cn\atd3\Session;
 use cn\atd3\MsgCenter;
 use cn\atd3\UserCenter;
+use cn\atd3\User;
 use cn\atd3\Api;
 use cn\atd3\ApiException;
 use cn\atd3\Token;
+use suda\tool\Value;
 
 /**
 * visit url /api/msg[/{action}] as all method to run this class.
@@ -28,11 +30,13 @@ class Msg extends \suda\core\Response
 {
     protected $mc;
     protected $uc;
+    protected $uid;
     protected $request;
     public function onRequest(Request $request)
     {
         $this->mc=new MsgCenter;
         $this->uc=new UserCenter;
+        
         $this->request=$request;
         if ($request->get()->client && $request->get()->token) {
             if (!$this->uc->checkClient($request->get()->client, $request->get()->token)) {
@@ -43,18 +47,46 @@ class Msg extends \suda\core\Response
         } else {
             return $this->json(['error'=>'no api client!']);
         }
+        $this->uid=(new User($this->uc))->getUserId();
+
         $action=$request->get()->action;
         $help=array(
-            'send',
-            'list',
-            'delete',
-            'get'
+            'send'=>[
+                'params'=> ['message', 'to'=>'int', 'type'=>['int', MsgCenter::TYPE_MESSAGE]],
+                'comments'=>'发送私信消息',
+            ] ,
+            'inbox'=>[
+                'params'=> ['type'=>['int', MsgCenter::TYPE_MESSAGE],'page'=>['int',1], 'count'=>['int',10]],
+                'comments'=>'获取消息列表',
+            ] ,
+            'delete'=>[
+                'params'=> ['ids'=>'array'],
+                'comments'=>'删除消息列表',
+            ] ,
         );
+
         try {
             // param values array
             $data=$request->isJson()?new Value($request->json()):($request->isPost()?$request->post():$request->get());
+            
             switch ($action) {
-                
+                case 'inbox':
+                    if (!$this->uid) {
+                        throw new ApiException('NoUserException', 'please Login');
+                    }
+                    return $this->json(['return'=>$this->mc->inbox($this->uid, $data->type(MsgCenter::TYPE_MESSAGE), $data->page(1), $data->count(10))]);
+                case 'send':
+                    if (!$this->uid) {
+                        throw new ApiException('NoUserException', 'please Login');
+                    }
+                    Api::check($data, ['message', 'to'=>'int', 'type'=>['int', MsgCenter::TYPE_MESSAGE]]);
+                    return $this->json(['return'=>$this->mc->send($this->uid, $data->to, $data->type, $data->message)]);
+                case 'delete':
+                    if (!$this->uid) {
+                        throw new ApiException('NoUserException', 'please Login');
+                    }
+                    Api::check($data, ['ids'=>'array']);
+                    return $this->json(['return'=>$this->mc->delete($this->uid, $data->ids)]);
                 default:return $this->json($help);
             }
         } catch (ApiException $e) {
